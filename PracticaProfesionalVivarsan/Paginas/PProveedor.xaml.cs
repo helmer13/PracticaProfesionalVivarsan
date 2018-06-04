a9638989a14a9a69b140df9f7506922ea1dfe6e6
helmer <sz.helmer@hotmail.com>
2018-06-04 06:16:00 -0600

    menu casi completo

---
+++ PracticaProfesionalVivarsan/Paginas/PProveedor.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Entidad;
+using Logica;
+namespace PracticaProfesionalVivarsan.Paginas
+{
+    /// <summary>
+    /// Interaction logic for PProveedor.xaml
+    /// </summary>
+    public partial class PProveedor : Page
+    {
+        public PProveedor()
+        {
+            InitializeComponent();
+        }
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            Proveedor proveedor = new Proveedor();
+            ProveedorLogica logica = new ProveedorLogica();
+
+            proveedor.Id = txtID.Text;
+            if (proveedor.Id == "")
+            {
+                proveedor.Id = Guid.NewGuid().ToString();
+            }
+
+
+            //  cliente.Id = Guid.NewGuid().ToString();
+            proveedor.NombreContacto = txtNombreContacto.Text;
+            proveedor.NombreProveedor = txtNombreProveedor.Text;
+            proveedor.Correo = txtCorreo.Text;
+            proveedor.Telefono = int.Parse(txtTelefono.Text);
+
+            logica.InsertarActialiarProveedor(proveedor);
+            Refrescar();
+            MessageBox.Show("Correcto.", "Advertencia");
+
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            txtID.Text = "";
+            txtNombreContacto.Text = "";
+            txtNombreProveedor.Text = "";
+            txtCorreo.Text = "";
+            txtTelefono.Text = "";
+        }
+        private void Refrescar()
+        {
+            ProveedorLogica logica = new ProveedorLogica();
+            List<Proveedor> lista = new List<Proveedor>();
+            lista = logica.obtenerProveedores();
+            dataGrid.ItemsSource = lista;
+        }
+
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Proveedor p = (Proveedor)dataGrid.SelectedCells[0].Item;
+            txtCorreo.Text = p.Correo;
+            txtID.Text = p.Id;
+            txtNombreContacto.Text = p.NombreContacto;
+            txtNombreProveedor.Text = p.NombreProveedor;
+            txtTelefono.Text = p.Telefono.ToString();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Refrescar();
+        }
+    }
+}
--- PracticaProfesionalVivarsan/frmMenu.xaml.cs
+++ PracticaProfesionalVivarsan/frmMenu.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Shapes;
 
 using Entidad;
 using Logica;
+using PracticaProfesionalVivarsan.Paginas;
+using System;
 
 namespace PracticaProfesionalVivarsan
 {
@@ -23,11 +25,33 @@ namespace PracticaProfesionalVivarsan
             InitializeComponent();
         }
 
-        private void Label_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void button1_Click(object sender, RoutedEventArgs e)
         {
-            frmProducto frmProducto = new frmProducto();
-            frmProducto.Show();
-            this.Close();
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+        }
+
+        private void mantProducto_Click(object sender, RoutedEventArgs e)
+        {
+           // PProducto pProducto = new PProducto();
+            frame.Content= new PProducto();
+        }
+
+        private void mantProveedor_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new PProveedor();
+        }
+
+        private void mantCliente_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new PCliente();
+        }
+
+        private void mantUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new PUsuario();
         }
     }
 }
