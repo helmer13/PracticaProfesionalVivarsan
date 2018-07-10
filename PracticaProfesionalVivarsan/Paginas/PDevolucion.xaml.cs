9a5345e9b97e4d6368efa8e70b21c1c83b89977b
helmer <sz.helmer@hotmail.com>
2018-07-10 00:58:30 -0600

    DEVOLUCION

---
+++ PracticaProfesionalVivarsan/Paginas/PDevolucion.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaProfesionalVivarsan.Paginas
+{
+    /// <summary>
+    /// Interaction logic for PDevolucion.xaml
+    /// </summary>
+    public partial class PDevolucion : Page
+    {
+        public PDevolucion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscarFactura_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
